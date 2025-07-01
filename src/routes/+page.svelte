bd54022f28785dd6ebe2e7cad168d5d66854e664
25tal <72045932+51talcol21@users.noreply.github.com>
2025-07-01 08:24:00 -0700

    Github Pages Setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="font-bold text-3xl">Hi! This is my personal website</h1>
+<p>Created using <a href="https://svelte.dev/docs/kit">sveltekit</a> and styled using 
+    <span class="italic"> 
+        Tailwind
+    </span>
+</p>
