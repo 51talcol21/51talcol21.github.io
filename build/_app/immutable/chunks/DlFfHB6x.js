392346bb0a9713c394a3784e3b6f746d071c2b89
25tal <72045932+51talcol21@users.noreply.github.com>
2025-07-11 08:40:46 -0700

    Added build to get ignore

--- build/_app/immutable/chunks/DlFfHB6x.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_zeo4c7)==null?void 0:s.base)??"/51talcol21.github.io";var e;const a=((e=globalThis.__sveltekit_zeo4c7)==null?void 0:e.assets)??t;export{a,t as b};
--- build/_app/immutable/nodes/3.CS4oJthk.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/CWj6FrbW.js";import"../chunks/Cs4lquYu.js";import{j as n,k as p,l as a}from"../chunks/BBYjtgcD.js";function m(t){a();var o=n("Workin on it!");p(t,o)}export{m as component};
